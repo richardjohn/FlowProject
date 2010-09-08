9c94ab835e248f07917344ee92a85a26ac01b63c
hashinpanak <hashinp@gmail.com>
2010-09-08 22:34:59 +0100

    components

---
+++ lib/Flow/Cart/Cart.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hashinpanakkaparambil
+ */
+interface Cart {
+    //put your code here
+}
+?>
---
+++ lib/Flow/Enterprise/Service/Service.php
@@ -0,0 +1,13 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hashinpanakkaparambil
+ */
+interface Service {
+    //put your code here
+}
